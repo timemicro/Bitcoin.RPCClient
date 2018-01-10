8c71d2aef952ae213719a10543f2dd809a862290
mattf <matt.fang@outlook.com>
2018-01-10 16:48:46 +0800

    GetChainTips

---
+++ src/Timemicro.Bitcoin.RPCClient/Methods/GetChainTipsParams.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Timemicro.Bitcoin.RPCClient.Methods
+{
+    public class GetChainTipsParams : JsonRPCRequestParams
+    {
+        public GetChainTipsParams()
+        {
+        }
+    }
+}
---
+++ src/Timemicro.Bitcoin.RPCClient/Methods/GetChainTipsResponse.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Timemicro.Bitcoin.RPCClient.Methods
+{
+    public class GetChainTipsResponse : JsonRPCResponse<List<ChainTip>>
+    {
+        public GetChainTipsResponse()
+        {
+        }
+    }
+
+    public class ChainTip
+    {
+        [JsonProperty("height")]
+        public int Height { get; set; }
+
+        [JsonProperty("hash")]
+        public string Hash { get; set; }
+
+        [JsonProperty("branchlen")]
+        public int BranchLen { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+
+    public static class ChainTipStatus
+    {
+        public static string Active = "active";
+
+        public static string Invalid = "invalid";
+
+        public static string HeadersOnly = "headers-only";
+
+        public static string ValidHeaders = "valid-headers";
+
+        public static string ValidFork = "valid-fork";
+
+        public static string Unknown = "unknown";
+
+    }
+}
